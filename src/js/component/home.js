33fb543b1492424bc28b1804da10300febeac6b4
paocastrob <paocastrob@gmail.com>
2019-07-25 21:11:56 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,82 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	tablero = {
+		a1: "",
+		a2: "",
+		a3: "",
+		b1: "",
+		b2: "",
+		b3: "",
+		c1: "",
+		c2: "",
+		c3: ""
+	};
+	player = "0";
+	enElClick(e) {
+		e.target.innerHTML = this.player;
+		if (this.player === "0") {
+			this.player = "x";
+			// block of code to be executed if the condition is true
+		} else {
+			this.player = "0";
+			// block of code to be executed if the condition is false
+		}
+	}
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="outerbox">
+				<div className="row">
+					<div
+						id="a1"
+						className="col border border border-dark border-left-0 border-top-0"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="a2"
+						className="col border border border-dark border-top-0"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="a3"
+						className="col border border border-dark border-right-0 border-top-0"
+						onClick={e => this.enElClick(e)}
+					/>
+				</div>
+				<div className="row">
+					<div
+						id="b1"
+						className="col border border border-dark border-left-0"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="b2"
+						className="col border border border-dark"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="b3"
+						className="col border border border-dark border-right-0"
+						onClick={e => this.enElClick(e)}
+					/>
+				</div>
+				<div className="row">
+					<div
+						id="c1"
+						className="col border border border-dark border-left-0 border-bottom-0"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="c2"
+						className="col border border border-dark border-bottom-0"
+						onClick={e => this.enElClick(e)}
+					/>
+					<div
+						id="c3"
+						className="col border border border-dark border-right-0 border-bottom-0"
+						onClick={e => this.enElClick(e)}
+					/>
+				</div>
 			</div>
 		);
 	}
