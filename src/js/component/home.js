33de152ca9150f84c47586c77011e67b88eb25ad
paocastrob <paocastrob@gmail.com>
2019-07-25 21:28:22 +0000

    second commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -33,51 +33,51 @@ export class Home extends React.Component {
 				<div className="row">
 					<div
 						id="a1"
-						className="col border border border-dark border-left-0 border-top-0"
+						className="col border border border-dark border-left-0 border-top-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="a2"
-						className="col border border border-dark border-top-0"
+						className="col border border border-dark border-top-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="a3"
-						className="col border border border-dark border-right-0 border-top-0"
+						className="col border border border-dark border-right-0 border-top-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 				</div>
 				<div className="row">
 					<div
 						id="b1"
-						className="col border border border-dark border-left-0"
+						className="col border border border-dark border-left-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="b2"
-						className="col border border border-dark"
+						className="col border border border-dark text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="b3"
-						className="col border border border-dark border-right-0"
+						className="col border border border-dark border-right-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 				</div>
 				<div className="row">
 					<div
 						id="c1"
-						className="col border border border-dark border-left-0 border-bottom-0"
+						className="col border border border-dark border-left-0 border-bottom-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="c2"
-						className="col border border border-dark border-bottom-0"
+						className="col border border border-dark border-bottom-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 					<div
 						id="c3"
-						className="col border border border-dark border-right-0 border-bottom-0"
+						className="col border border border-dark border-right-0 border-bottom-0 text-center pt-5"
 						onClick={e => this.enElClick(e)}
 					/>
 				</div>
